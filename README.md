97d436856bd0f0762f69022d9431933db4ed7846
JBQlg <jb.quagliaroli@gmail.com>
2024-10-16 22:04:00 +0200

    add documentation

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 
+## Projet de simulation de réservation de vols - KATA Onepoint
 
+Un code permettant de simuler un système de réservation de vol avec attribution des places dans un avion. 
 
+Le code devra permettre : 
 
-### avancement étape par étape
+* La définition et la gestion de plusieurs types d’avion (taille, disposition des sièges) 
+* L’attribution des sièges à un ou plusieurs passagers en tenant compte des sièges déjà attribués 
+* La modification du siège d’un ou plusieurs passagers 
+* L’impression des détails de la réservation (numéro de vol + siège) d’un ou plusieurs passagers. Bien que cela ne soit pas explicitement pris en compte dans l’évaluation, la possibilité d’exécuter le code en ligne de commande serait un plus. Le candidat devra mettre en œuvre autant que possible les meilleurs pratiques de développement (organisation du code, syntaxe, documentation du code, tests unitaires) 
+
+---
+
+### Fonctionnement du code
+    Le developement de ce code à partir de ce cahier des charges a été fait avec pour commencer une approche de base fonctionnant avec des menus contextuels.
+    Ensuite, une approche avec des commandes directes a été ajoutée pour permettre une utilisation plus fluide du code.
+    Enfin, une persistance des données a été ajoutée pour permettre de sauvegarder les données des vols et des passagers.
 
 
 #### Approches de développement
-    - approche avec menu contextuel
+    - approche de base avec menu contextuel
     - approche avec commande directe 
-    - 
-#### test
-    - ajouts des tests unitaires des différentes fonctions
-    - mise en place d'une CI pour les tests
 
 #### persistance des données 
     - approche avec JSON
     - approche avec SQLite
   
-#### Premier schéma de verision 
-#### second scéma de version
-#### troisième schéma de version avec simplification
+---
+
+Les différentes notes de conceptions expliquant les différentes approches de développement et les choix de conception sont disponibles dans le dossier `documentation`.
---
+++ tests/tests_flight_handler.py
@@ -0,0 +1,37 @@
+import unittest
+from ..booking_system.flight_handler import Flight_handler
+from ..booking_system.flight import Flight
+from ..booking_system.seat import Seat
+
+
+class TestFlightHandler(unittest.TestCase):
+    
+    def setUp(self):
+        self.flight_handler = Flight_handler()
+        
+    def test_create_flight(self):
+        """Flight creation test"""
+        self.flight_handler.create_flight(plane_id="PL123", departure="Paris", destination="New York", schedule="10:00", date="2022-01-01", reservations=[])
+        # Check if the flight has been created
+        self.assertEqual(len(self.flight_handler.flights), 1)
+        # Check if the flight has the right attributes
+        self.assertEqual(len(self.flight_handler.flights), 1)
+        self.assertEqual(self.flight_handler.flights[0].departure, "Paris")
+        self.assertEqual(self.flight_handler.flights[0].arrival, "New York")
+        self.assertEqual(self.flight_handler.flights[0].schedule, "10:00")
+        self.assertEqual(self.flight_handler.flights[0].date, "2022-01-01")
+        self.assertEqual(self.flight_handler.flights[0].plane_id, "PL123")
+        self.assertEqual(self.flight_handler.flights[0].reservations, [])
+        
+    def delete_flight(self):
+        """Flight delete test"""
+        flight = self.flight_handler.create_flight(plane_id="PL123", departure="Paris", destination="New York", schedule="12:00", date="2024-05-01", reservations=[])
+        self.flight_handler.delete_flight(flight.id)
+        self.assertEqual(len(self.flight_handler.flights), 0)
+                    
+    def test_get_flight(self):
+        """Test find flight by ID"""
+        flight = self.flight_handler.create_flight(plane_id="PL123", departure="Paris", destination="New York", schedule="12:00", date="2024-05-01", reservations=[])
+        found_flight = self.flight_handler.get_flight(flight.id)
+        self.assertEqual(found_flight, flight)
+        self.assertIsNone(self.flight_handler.get_flight("INVALID_ID"))
